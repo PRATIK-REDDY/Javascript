6d262f2a15fe0457e099e9aa78bca45d174cb16c
R PRATIK REDDY <100156512+PRATIK-REDDY@users.noreply.github.com>
2023-09-22 11:24:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+// Here you learn about variable
+
+// In js basicaly you declear the variables in three ways
+
+// 1. using const which you cannot reinisilised after ones initialize
+const email = "reddy@123"
+
+// 2. using let
+let name = "pratik"
+
+// 3. using var
+var city = "berhampur"
+
+/* 
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+// you can print using console
+console.log(email)
+
+// Here you learn thing print all the variable values in a tabular form
+console.table([email, name, city]) 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict" // this syntex treat all JS code as newer version (means all the code which you write is treat or follow the new version)
+
+// alert(1 + 2) (This will throw an error because we are using here nodejs, not browser)
+
+// Most inmportant in JS ; and space is not mandatory
+// another benifit is it imporve the readability
+
+
+// Datatypes in JS
+
+// These are premetive datatypes:-
+
+// 1. number            => range is (2 to power 53)
+// 2. bigint
+// 3. string            => ""
+// 4. boolean           => true/false
+// 5. null              => standalone value
+// 6. undefined         => still know no value is assign to that variable
+// 7. symbol            => this datatype is used to findout uniqueness in components mostly use in (Reactjs technology)
+
+
+// Non-premitive datatypes:-
+// 1. object
+
+
+// if you want to findout datatype of any value then....
+
+console.log(typeof "partik")
+
+// interview Q is datatype of null -------- that is object type and undefined type is undefined
+console.log(typeof null)
+console.log(typeof undefined)
