5bf3505635f397146c00d54a44f4e7068c291d89
R PRATIK REDDY <100156512+PRATIK-REDDY@users.noreply.github.com>
2023-09-22 12:39:31 +0000

    Operation

--- 01_basics/03_conversionandOperation.js
+++ 01_basics/03_conversionandOperation.js
@@ -2,7 +2,7 @@
 
 // for example
 
-let score = "pratik"
+// let score = "pratik"
 
 // two ways to checking of type of value
 // 1. console.log(typeof score)
@@ -10,10 +10,12 @@ let score = "pratik"
 // 2. This is using function
 // console.log(typeof(score))
 
-// here you preform the conversion
-let covertedvalue = Number(score)
 
-console.log(covertedvalue)
+// here you preform the conversion
+// let covertedvalue = Number(score)
+
+// console.log(covertedvalue)
+
 
 // it will give (NaN) it is also a special type
 
@@ -23,3 +25,38 @@ console.log(covertedvalue)
 // if you convert --- "33"      => 33 (number)
 // if you convert --- "33abc"   => NaN
 //                      true    => 1;   false   => 0
+
+
+
+// ===================== Here you learn about operation ================
+
+// let value = 9
+// let negValue = -value
+
+// Here you assign -ve value therefore the value is -ve simple
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2) // This means power 2 to the power 2
+// console.log(2 / 2)
+// console.log(2 % 2)  // for reminder
+
+
+// here you learn about tricky thinks
+
+// string concatination
+// let str1 = "pratik"
+// let str2 = "reddy"
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+
+// tricky
+
+console.log("2" + 3)        // 23
+console.log("2" + 3 + 1)    // 231
+console.log(2 + 3 + "1")    // 51 
