d3e697a62c368685d349dd6b39b53223e56c6ec2
R PRATIK REDDY <100156512+PRATIK-REDDY@users.noreply.github.com>
2023-09-23 11:24:03 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,32 @@
+// Here you learn about comparision in JS
+
+// some basic comparision which having simple answer
+
+// simple comparision because both the operant datatype are same
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// Here you compare with different datatype operant
+
+// console.log("2" > 1)
+
+
+
+// such type of comparision you much avoid in JS such as
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+
+
+// important question for interview diff between == and ===
+
+// ==   (only compare the values of operants)
+// ===  (not only check the values but also check the datatype).
