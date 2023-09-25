fb4cce980e7e3b2065c33fb7067782630ec726b1
R PRATIK REDDY <100156512+PRATIK-REDDY@users.noreply.github.com>
2023-09-25 07:42:10 +0000

    StringinJs

---
+++ 02_stirng/01_string.js
@@ -0,0 +1,13 @@
+// Here you learn about string in Js
+
+// string mean a series of character which you write in between '' or "".
+
+// for ex - small operation you will do
+
+// const n1 = "pratik"
+
+// const n2 = "reddy"
+
+// console.log(n1 + n2)
+
+
