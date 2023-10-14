254be198f29c4aa5d11a01fd0cd6f1951180d5ff
R PRATIK REDDY <100156512+PRATIK-REDDY@users.noreply.github.com>
2023-10-14 14:32:35 +0000

    funtion_basic

---
+++ 05_function/01_Basic.js
@@ -0,0 +1,23 @@
+// Here you learn about function
+
+function print() {
+    console.log("P");
+    console.log("r");
+    console.log("t");
+    console.log("i");
+    console.log("k");
+}
+
+// function constain 
+// 1. function (keyword)
+// 2. name of the function along with () where you pass the formal argument
+
+
+// this is function Reference (means address)
+// console.log(print);  // nothing will hanppen 
+
+// if you call the function like
+// console.log(print());
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
