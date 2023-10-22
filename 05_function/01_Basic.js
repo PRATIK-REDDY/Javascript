ed44a0958e90c0700a3875062c238588c431f097
R PRATIK REDDY <100156512+PRATIK-REDDY@users.noreply.github.com>
2023-10-22 18:35:51 +0000

    moreabout_function

--- 05_function/01_Basic.js
+++ 05_function/01_Basic.js
@@ -21,3 +21,45 @@ function print() {
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
+
+function add(a, b) {
+    let result;
+
+    result = a + b
+    // if you not use the return keyword it show undefine (return is important in Js)
+    return result
+}
+
+
+let result = add(1, 2);
+
+// console.log(result);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// here you learn about important operator know as rest and sprade operator ( look like (...)) it behave different way in diff places.
+// Here (...) operator works is to convert all the values given by the user into array
+
+function cartApp(...product) {
+    return product
+}
+
+// console.log(cartApp(1, 2, 3, 4));
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// here you preform object with function
+
+const objectname = {
+    name: "pratik",
+    lnumber: 18
+}
+
+
+// deal with function
+function objectFunction(objectname) {
+    console.log(`Username is ${objectname.name} and locky number is ${objectname.lnumber}`);
+}
+
+// objectFunction(objectname)
