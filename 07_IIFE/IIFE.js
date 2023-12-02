f8e39c538648f68167c773e49f89b8c3215653a3
R PRATIK REDDY <pratikreddy0000@gmail.com>
2023-12-02 23:54:00 +0530

    IIFE function

---
+++ 07_IIFE/IIFE.js
@@ -0,0 +1,4 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+// sometime the globalscope( mean global variable ) make disterbance( mean problem of some function ) to restrict this you use -----> IIFE
+
