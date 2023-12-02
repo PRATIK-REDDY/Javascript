6f6362680abc68f95f9b73c7dd57ca498fea3c36
R PRATIK REDDY <pratikreddy0000@gmail.com>
2023-12-03 00:53:11 +0530

    more about IIFE

--- 07_IIFE/IIFE.js
+++ 07_IIFE/IIFE.js
@@ -1,4 +1,26 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-// sometime the globalscope( mean global variable ) make disterbance( mean problem of some function ) to restrict this you use -----> IIFE
+// 1. sometime the globalscope( mean global variable ) make disterbance( mean problem of some function ) to restrict this you use -----> IIFE
+
+// 2. and another resion is if you want to execute any function innediately then use IIFE ( like database connection function )
+
+
+// you write the IIFE function by using ()()
+
+// 1. () -----> contain function
+// 2. () -----> means function calling (execution you can pass the argument also)
+
+(function print() {
+    console.log("my name is pratik");
+})();
+
+// but if you write morethen one IIFE function then use ';' to end one IIFE function otherwise you get a problem
+
+// we also use arrow fun in IIFE
+
+((name) => {
+    console.log(`my brother name is ${name}`);
+})('pradyot')
+
+
 
